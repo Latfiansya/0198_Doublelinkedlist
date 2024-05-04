8a6a27e472d07485292206e042c338073abc71e5
Latfiansya <latfiansyadzikri@gmail.com>
2024-05-05 00:31:08 +0700

    Operasi dan pengondisian node ketika ada penambahan data baru

--- 198_DoubleLinkedList/198_DoubleLinkedList.cpp
+++ 198_DoubleLinkedList/198_DoubleLinkedList.cpp
@@ -20,4 +20,52 @@ void addNode()
     cout << "\nEnter the name of the student: ";
     cin >> newNode->name;       // Assign value to the date field of the new node 
 
+    // Insert the new code in the list
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    {
+        // step 2 insert the new node at the beginning
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033]0m" << endl;
+            return;
+        }
+        // If the list is empty, make the new node the START
+        newNode->next = START; // step 3: make the new node point to the first node
+        if (START != NULL)
+        {
+            START->prev = newNode; // step 4: make the first node point to the new node
+        }
+        newNode->prev = NULL;      // step 5: make the new node point to the NULL
+        START = newNode;    // step 6: make the new node the first node
+    }
+    else
+    {
+        // insert the new node in the middle or at the end
+        Node* current = START; // step 1.a: start from the first node
+        Node* previous = NULL; // step 1.b: previous node is NULL initially
+
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {                               // step 1.c: traverse the list to find the corrrect position
+            previous = current;         // step 1.d: move the previous to the current node
+            current = current->next;    // step 1.e: move the current to the next node
+        }
+
+        newNode->next = current;    // step 4: Make the next field of the new node point to current
+        newNode->prev = previous;   // step 5: Make the previous field of the new node point to previous
+
+        if (current != NULL)
+        {
+            current->prev = newNode;  // step 6: Make the previous field of the current node point to the new node
+        }
+
+        if (previous != NULL)
+        {
+            previous->next = newNode;  // step 7: Make the next field of the previous node point to the new node
+        }
+        else
+        {
+            // if previous is still NULL, it means newNode is now the first node
+            START = newNode;
+        }
+    }
 }
