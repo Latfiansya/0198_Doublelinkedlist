496ed6fb13f346e0281c6e2439e5e0508c7a0e38
Latfiansya <latfiansyadzikri@gmail.com>
2024-05-05 00:36:08 +0700

    Deklarasi prosedur deleteNode untuk menghapus node yang sudah dibuat

--- 198_DoubleLinkedList/198_DoubleLinkedList.cpp
+++ 198_DoubleLinkedList/198_DoubleLinkedList.cpp
@@ -81,3 +81,16 @@ bool search(int rollNo, Node** previous, Node** current)
     }
     return (*current != NULL);
 }
+
+void deleteNode()
+{
+    Node* previous, * current; //
+    int rollNo;
+
+    cout << "\nEnter the roll number of the student whose record is tobe deleted";
+    cin >> rollNo; // step 3: get the roll number to be deleted
+
+}
+
+
+
